0e2f9539118d1451d8d6046c9802fbfcec5159b8
pushkal <mudhapakapushkal@gmail.com>
2024-09-01 22:44:25 -0400

    initial commit

---
+++ src/main/java/com/split/split_be/SplitBeApplication.java
@@ -0,0 +1,13 @@
+package com.split.split_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/split/split_be/SplitBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.split.split_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
